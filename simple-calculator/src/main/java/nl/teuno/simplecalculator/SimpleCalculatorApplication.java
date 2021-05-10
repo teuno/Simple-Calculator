1394e1fd4375319a930cfec259fab4c7237693cd
thooijer <thooijer@quintor.nl>
2021-05-10 10:00:40 +0200

    added initial commit

---
+++ simple-calculator/src/main/java/nl/teuno/simplecalculator/SimpleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package nl.teuno.simplecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCalculatorApplication.class, args);
+	}
+
+}
---
+++ simple-calculator/src/test/java/nl/teuno/simplecalculator/SimpleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.teuno.simplecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
